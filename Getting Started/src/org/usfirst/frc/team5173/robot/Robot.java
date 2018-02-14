b61878e30d0d6bfdfd2a6bb28550cdf3025d21d0
Fajigajiga <diogeneslopez34@gmail.com>
2018-02-14 18:08:21 -0500

    Newest Code

--- Getting Started/src/org/usfirst/frc/team5173/robot/Robot.java
+++ Getting Started/src/org/usfirst/frc/team5173/robot/Robot.java
@@ -1,5 +1,4 @@
 package org.usfirst.frc.team5173.robot;
-
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.Encoder;
@@ -11,6 +10,7 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;;
 /**
  * This is a 
  * The VM is configured to automatically run this class, and to call the
@@ -20,13 +20,22 @@ import edu.wpi.first.wpilibj.GenericHID.Hand;
  * directory.
  */
 public class Robot extends IterativeRobot {
+
 	RobotDrive Robot = new RobotDrive(1, 2);
-	Encoder encoder = new Encoder(0, 1);
-	Spark Sparky = new Spark(0);
+	
+	Encoder encoder = new Encoder(0, 1, false, Encoder.EncodingType.k4X);
+
+	Spark Sparky_ForthBackith = new Spark(3);
+	
 	XboxController controller = new XboxController(0);
+	
 	Timer timer = new Timer();
+	
 	DoubleSolenoid Claw = new DoubleSolenoid(1,2);
+	
 	Compressor c = new Compressor(1);
+	
+	Spark Voltage_Lift = new Spark (4);
 	{
 		
 	c.start();
@@ -39,18 +48,22 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void robotInit() {
-		//Temporary number for now, look up the pulses per revolution for encoder
-		double distancePerPulse = 0.01;
-		encoder.setDistancePerPulse(distancePerPulse);
+
+		encoder.setMaxPeriod(.1);
+		encoder.setMinRate(10);
+		encoder.setDistancePerPulse(1/1024 * 1.625 * Math.PI/35);
 	}
 
 	/**
 	 * This function is run once each time the robot enters autonomous mode
 	 */
 	@Override
-	public void autonomousInit() {	
+	public void autonomousInit() {
+		
 		timer.reset();
+		
 		timer.start();
+		
 	}
 
 	/**
@@ -58,39 +71,41 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void autonomousPeriodic() {
-		
+		//double distance = encoder.getDistance();
 		// Drive for 2 seconds
-		if (encoder.getDistance() < 2.0) {
-			Robot.drive(-0.5, 0.0); // drive forwards half speed
-		} else {
-			Robot.drive(0.0, 0.0); // stop robot
+		timer.start();
+		
+		if (timer.get() < 2.0){			
+			Robot.drive(-0.2, 0.0); // drive forwards half speed			
+		} else {			
+			Robot.drive(0.0, 0.0); // stop robot			
 		}
 		//restarts timer
-		timer.reset();
-		timer.start();
+		timer.reset();		
+		timer.start();		
 		// turn one direction for 2 seconds
-		if (timer.get()< 2.0) {
-			Robot.drive(0.0, -0.5);
-		} else { 
-			Robot.drive(0, 0);//stop
+		if (timer.get()< 2.0) {			
+			Robot.drive(0.0, -0.2);			
+		} else { 			
+			Robot.drive(0, 0);//stop			
 		}
 		// Restart Timer
-		timer.reset();
-		timer.start();
+		timer.reset();		
+		timer.start();		
 		// turn opposite direction for 2 seconds
-		if (timer.get() < 2.0) {
-			Robot.drive(0.0, 0.5);
-		} else {
-			Robot.drive(0.0, 0.0);// stop
+		if (timer.get() < 2.0) {			
+			Robot.drive(0.0, 0.2);			
+		} else {			
+			Robot.drive(0.0, 0.0);// stop			
 		} 
 		//Restart Timer
-		timer.reset();
-		timer.start();
+		timer.reset();		
+		timer.start();		
 		//drives backward for 2 seconds
-		if (timer.get() < 2.0) {
-			Robot.drive(0.5, 0.0);
-		} else {
-			Robot.drive(0.0, 0.0);//stop
+		if (timer.get() < 2.0) {			
+			Robot.drive(0.2, 0.0);			
+		} else {			
+			Robot.drive(0.0, 0.0);//stop			
 		}
 	}
 
@@ -108,14 +123,19 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void teleopPeriodic() {
+		
 		//Robot.arcadeDrive(controller);
+		
 		//Robot.arcadeDrive(controller.getY(Hand.kLeft), controller.getX(Hand.kLeft));
+		
 		//Robot.arcadeDrive(controller.getRawAxis(1), controller.getRawAxis(0));
+		//double distance = encoder.getDistance();
 		double rawY = controller.getRawAxis(1);
 		rawY = Math.pow(rawY, 3);
 		double rawX = controller.getRawAxis(0);
 		rawX = Math.pow(rawX, 3);
 		Robot.arcadeDrive(rawY, rawX);
+		
 		if(controller.getBButton() == true) {
 			Claw.set(DoubleSolenoid.Value.kForward);
 		}
@@ -123,34 +143,46 @@ public class Robot extends IterativeRobot {
 			Claw.set(DoubleSolenoid.Value.kReverse);
 		}
 		//double count = 0;
-		//while(controller.getBumper(Hand.kLeft) && encoder.getDistance() < 5) {
-			//Sparky.set(Math.pow(count, 3));
-			//count += 0.1;
-			//try {
-				//Thread.sleep(1000);
-			//} catch (InterruptedException ex) {
-				//Thread.currentThread().interrupt();
-			//}
-		}
+		if(controller.getBumper(Hand.kLeft) /*&& encoder.getDistance() < 5*/) {
+			Sparky_ForthBackith.set(0.75);
+			//Sparky.set(Math.pow(count, 2));
+			//count += 0.5;
+			/*try {
+				Thread.sleep(1000);
+			} catch (InterruptedException ex) {
+				Thread.currentThread().interrupt();
+			}*/
+		} else {
 		
 		//double count2 = 0;
-		//while(controller.getBumper(Hand.kRight) && encoder.getDistance() < 5) {
-			//Sparky.set(Math.pow(count2, 3));
-			//count -= 0.1;
-			//try {
-				//Thread.sleep(1000);
-			//} catch (InterruptedException ex) {
-				//Thread.currentThread().interrupt();
-			//}
-		//}
-	//	Sparky.set(0);
-	//    c.setClosedLoopControl(true);
-	//}
+			if(controller.getBumper(Hand.kRight) /*&& encoder.getDistance() < 5*/) {
+			Sparky_ForthBackith.set(-0.75);
+			//Sparky.set(Math.pow(count2, 2));
+			//count2 -= 0.5;
+			/*try {
+				Thread.sleep(1000);
+			} catch (InterruptedException ex) {
+				Thread.currentThread().interrupt();
+			}*/
+			} else { Sparky_ForthBackith.set(0.0); }
+		}
+		if(controller.getTriggerAxis(Hand.kLeft) > .7) {
+			Voltage_Lift.set(0.5);
+		} else {
+			if(controller.getTriggerAxis(Hand.kRight) > .7){ Voltage_Lift.set(-0.5);		
+				} else { Voltage_Lift.set(0.0);}
+		}
+	
+	    c.setClosedLoopControl(true);
+	}
+
 	/**
 	 * This function is called periodically during test mode
 	 */
 	@Override
 	public void testPeriodic() {
+		
 		LiveWindow.run();
+		SmartDashboard.putNumber("encoder Distance",encoder.getDistance());
 	}
 }
